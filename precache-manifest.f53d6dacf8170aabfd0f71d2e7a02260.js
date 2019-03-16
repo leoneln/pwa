e4a98226a3bc5cddfd07dd599c8442bad53300c6
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-03-16 07:37:40 -0600

    Updates

---
+++ precache-manifest.f53d6dacf8170aabfd0f71d2e7a02260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73cdf6db9551ad39ce82",
+    "url": "/leoneln/pwa.git/static/js/runtime~main.7b5dc25c.js"
+  },
+  {
+    "revision": "61ee49161d7ee39328b3",
+    "url": "/leoneln/pwa.git/static/js/main.1fdb70b8.chunk.js"
+  },
+  {
+    "revision": "cc13aa08cb2b0002b6e0",
+    "url": "/leoneln/pwa.git/static/js/2.8d7e50ad.chunk.js"
+  },
+  {
+    "revision": "61ee49161d7ee39328b3",
+    "url": "/leoneln/pwa.git/static/css/main.a80393e6.chunk.css"
+  },
+  {
+    "revision": "dfbbbe86e84cdcfda8366881fc35bb48",
+    "url": "/leoneln/pwa.git/index.html"
+  }
+];
